3bc9165d87527e26b5c900eb8ce594f8cd896a15
Sagarnil Das <sagarnildass@gmail.com>
2018-11-23 01:24:19 +0530

    User model changes SQLAlchemy

--- section_6_sqlalchemy/code/models/user.py
+++ section_6_sqlalchemy/code/models/user.py
@@ -10,45 +10,19 @@ class UserModel(db.Model):
     password = db.Column(db.String(80))
 
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = 'SELECT * FROM users WHERE username = ?'
-        result = cursor.execute(query, (username,))
-        #get the first row
-        row = result.fetchone()
-        #if row is not None
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = 'SELECT * FROM users WHERE id = ?'
-        result = cursor.execute(query, (_id,))
-        #get the first row
-        row = result.fetchone()
-        #if row is not None
-        if row:
-            #user = cls(row[0], row[1], row[2])
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
